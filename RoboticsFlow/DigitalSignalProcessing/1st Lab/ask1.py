e48334e93fd74e4b131b7e8d64e4945805b6dcb6
thodpap <paparrigopoulosthodoris@gmail.com>
2021-03-22 20:26:25 +0200

    Fix d array

--- RoboticsFlow/DigitalSignalProcessing/1st Lab/ask1.py
+++ RoboticsFlow/DigitalSignalProcessing/1st Lab/ask1.py
@@ -1,6 +1,5 @@
 import numpy as np
-import math
-import simpleaudio as sa
+import math 
 
 n = np.array(range(1,1001))
 
@@ -8,23 +7,15 @@ frequencies_column    = [0.9273, 1.0247, 1.1328]
 frequencies_row = [0.5346, 0.5906, 0.6535, 0.7217]
 
 # Let our signals be named d[n]
-
-numbers = []
-for i in range(1,10):
-	numbers.append(i)
-numbers.append(0)
-
 d = []
-for N in n:
-	temp = []
-	for row in frequencies_row:
-		for column in frequencies_column:
-			if row == 0.7217 and (column == 0.9273 or column == 1.1328) :
-				continue
+d.append(np.sin(0.7217 * n) + np.sin(1.0247 * n)) # d[0]
+for row in frequencies_row:
+	for column in frequencies_column:
+		if row == 0.7217:
+			continue
+		d.append(np.sin(row * n) + np.sin(column * n))
 
-			temp.append(math.sin(row * N) + math.sin(column * N))
-	d.append(temp)
 
-# print(d)
-# print(d[0])
-playsound(d[0])
+
+ 
+# print(d[0]) 

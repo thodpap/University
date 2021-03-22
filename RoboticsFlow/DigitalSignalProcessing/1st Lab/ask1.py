dbd69d848ae91f4602ad405ec68f3895aea81153
thodpap <paparrigopoulosthodoris@gmail.com>
2021-03-22 22:15:43 +0200

    Fft with hamming 1st step

--- RoboticsFlow/DigitalSignalProcessing/1st Lab/ask1.py
+++ RoboticsFlow/DigitalSignalProcessing/1st Lab/ask1.py
@@ -3,6 +3,7 @@ import math
 import pylab  
 from scipy.io.wavfile import write 
 from scipy.signal import get_window
+from scipy.signal import find_peaks
 
 N = 1000
 n = np.array(range(1,1001))
@@ -65,11 +66,57 @@ for index, number in enumerate(phone_number):
 phone_number_array = np.array(phone_number_array)
 write("number.wav", total_frequency, phone_number_array)
 
-window = get_window('hamming', N)
+window = get_window('hamming',N)
 
 pylab.figure(2)
-pylab.plot(n,window)
+pylab.plot(np.arange(N),window)
 
 pylab.figure(3)
 pylab.plot(np.arange(8700), phone_number_array)
+
+
+#############################################################
+hamming_result = []
+for index, number in enumerate(phone_number):
+	for index2, d_i in enumerate(d[number]):
+		hamming_result.append(d_i * window[index2]) 
+	if index != 7: 
+		for temp in range(100):
+			hamming_result.append(0)
+
+pylab.figure(4)
+pylab.plot(np.arange(8700), hamming_result)
+
+
+
+###################################################
+# FFT #
+
+fft_with_hamming = []
+
+for i in range(8):
+	start,last = i*1000 + 100*i, (i+1)*1000 + 100*(i+1)
+	tmp_arr = hamming_result[start:last]
+	fft_with_hamming.append(np.fft.fft(tmp_arr))
+
+pylab.figure(5)
+pylab.plot(np.arange(1100), fft_with_hamming[0])
+
+peaks_array = []
+
+for i in range(8): 
+	peaks_array.append(find_peaks(fft_with_hamming[i]))
+
+
+print(np.array(peaks_array))
+
+
+
+
+
+
+
+
+
+
 pylab.show()

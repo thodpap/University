73a3a63cf085a32631ab3d9f9ef13c5393ad86db
thodpap <paparrigopoulosthodoris@gmail.com>
2021-03-26 14:42:31 +0200

    ask2 full in C++

---
+++ SoftwareFlow/Programming Languages I/1η Σειρά/loop_rooms.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+char arr[1000][1000];
+int visited[1000][1000];
+int N,M;
+int counter;
+
+void find_parents(int i,int j) {
+	visited[i][j] = 1;
+	++counter;
+	// there are 4 possible parents 
+	if (j >= 1 && !visited[i][j-1] && arr[i][j-1] == 'R')
+		find_parents(i,j - 1);
+	if (j < M  && !visited[i][j+1] && arr[i][j+1] == 'L')
+		find_parents(i,j + 1);
+	if (i >= 1 && !visited[i-1][j] && arr[i-1][j] == 'D')
+		find_parents(i - 1,j);
+	if (i < N  && !visited[i+1][j] && arr[i+1][j] == 'U')
+		find_parents(i + 1,j);
+}
+
+void dfs() {
+	int p,q;
+
+	/* i = 0   and j 	     */
+	p = 0;
+	for(int j = 0; j < M; ++j) 
+		if (arr[p][j] == 'U') 
+			find_parents(q,j);
+
+	/* i = N-1 and j 	     */
+	p = N-1;
+	for (int j = 0; j < M; ++j) 
+		if (arr[p][j] == 'D') 
+			find_parents(p,j);
+
+	/* i 	   and j = 0 	 */
+	q = 0;
+	for (int i = 0; i < N; ++i) 
+		if (arr[i][q] == 'L') 
+			find_parents(i,q);
+
+	/* i 	   and j = M - 1 */
+	q = M - 1;
+	for (int i = 0; i < M; ++i) 
+		if (arr[i][q] == 'R') 
+			find_parents(i,q);
+			
+} 
+int main(){
+	cin >> N >> M;
+	for(int i = 0; i < N; ++i) 
+		for (int j = 0; j < N; ++j) 
+			cin >> arr[i][j];
+	dfs();
+	cout << (N*M - counter) << endl;
+
+}

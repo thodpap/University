92f073084ba01ec7124d21137e12bd523baf5512
thodpap <paparrigopoulosthodoris@gmail.com>
2021-05-25 00:40:58 +0300

    Add dictionary python qssort

--- SoftwareFlow/Programming Languages I/2nd/Python/qssort.py
+++ SoftwareFlow/Programming Languages I/2nd/Python/qssort.py
@@ -1,3 +1,4 @@
+from os import confstr
 import sys
 
 with open(sys.argv[1]) as f:
@@ -6,7 +7,11 @@ with open(sys.argv[1]) as f:
 
     queue = [int(x) for x in next(f).split()]  
 
+
 def BFS(queue, N):
+    def getState(queue,stack):
+        return str(queue) + str(stack)
+     
     def Q(queue, stack, path):  
         newStack = stack
         newStack.append(queue[0])
@@ -30,6 +35,10 @@ def BFS(queue, N):
         states = []
         states.append([queue, [], ''])
  
+        dictionary = dict()
+ 
+        dictionary[getState(queue, [])] = 1
+
         while True: 
             newStates = []
             for queue, stack, path in states:  
@@ -37,22 +46,45 @@ def BFS(queue, N):
                     if path == '':
                         path = 'empty'
                     return path
+                
 
                 if not stack:  
                     tempQ = Q(queue,stack,path) 
+                    obj = getState(tempQ[0],tempQ[1])
+                    if obj in dictionary:
+                        continue
+
+                    dictionary[obj] = 1
                     newStates.append([tempQ[0],tempQ[1],tempQ[2]])
+
                 elif not queue: 
                     tempS = S(queue,stack,path)
+                    obj = getState(tempS[0],tempS[1])
+
+                    if obj in dictionary:
+                        continue
+
+                    dictionary[obj] = 1
+
                     newStates.append([tempS[0],tempS[1],tempS[2]])
                 else: 
                     queueQ, stackQ, pathQ = Q(queue, stack, path) 
-                    queueS, stackS, pathS = S(queue, stack[:-1], path)
-                    
-                    newStates.append([queueQ, stackQ, pathQ ])
-                    newStates.append([queueS, stackS, pathS]) 
+
+                    obj = getState(queueQ,stackQ)
+                    if not obj in dictionary:  
+                        dictionary[obj] = 1
+
+                        newStates.append([queueQ, stackQ, pathQ ])
+
+                    queueS, stackS, pathS = S(queue, stack[:-1], path) 
+                    obj = getState(queueS,stackS)
+                    if not obj in dictionary:
+                        dictionary[obj] = 1
+
+                        newStates.append([queueS, stackS, pathS]) 
             
-            states = newStates 
+            states = newStates  
 
     return solver(queue, N)        
  
-print(BFS(queue, N))  
+print(BFS(queue, N))   

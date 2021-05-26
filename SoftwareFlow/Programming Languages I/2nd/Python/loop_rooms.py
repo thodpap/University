0dc00c085973f200cb363271f046b029ef942461
thodpap <paparrigopoulosthodoris@gmail.com>
2021-05-26 14:11:12 +0300

    Fix loop rooms and qssort to pass grader

--- SoftwareFlow/Programming Languages I/2nd/Python/loop_rooms.py
+++ SoftwareFlow/Programming Languages I/2nd/Python/loop_rooms.py
@@ -1,17 +1,17 @@
 import sys
-from typing import Counter
-import numpy as np
-from numpy.core.defchararray import count, find
+import numpy as np 
+
+#print(sys.getrecursionlimit())
+sys.setrecursionlimit(6000)
+
+
+visited = np.zeros((10000,10000))
 
 with open(sys.argv[1]) as f:
     N, M = [int(x) for x in next(f).split()] # read first line
 
-    arr = [ [x for x in line.split()] for line in f]
-
-print(N,M)
-print(arr) 
-
-visited = np.zeros( (N,M) )
+    arr = [ [x for x in line.split()] for line in f] 
+arr = np.array(arr)
 counter = 0
 def dfs(): 
     def find_parents(i,j):
@@ -23,18 +23,17 @@ def dfs():
 
         visited[i][j] = 1
         counter += 1
-        print (arr[i][0][j])
         
         if j >= 1 and visited[i][j - 1] == 0 and arr[i][0][j - 1] == "R":
             find_parents(i, j - 1)
         
-        if j < M - 1 and visited[i][j + 1] == 0 and arr[i][0][j + 1] == "L":
+        if j + 1 < M and visited[i][j + 1] == 0 and arr[i][0][j + 1] == "L":
             find_parents(i, j + 1)
         
         if i >= 1 and visited[i - 1][j] == 0 and arr[i - 1][0][j] == "D":
             find_parents(i - 1, j) 
 
-        if i < N - 1 and visited[i + 1][j] == 0 and arr[i + 1][0][j] == "U":
+        if i + 1 < N and visited[i + 1][j] == 0 and arr[i + 1][0][j] == "U":
             find_parents(i + 1,j)
    
     def searchHorizontal(p, char):
@@ -58,7 +57,7 @@ def dfs():
     global M 
      
     searchHorizontal(0,'U')
-    searchHorizontal(N-1,'U')
+    searchHorizontal(N-1,'D')
     
     searchVertical(0, 'L')
     searchVertical(M - 1, 'R')
